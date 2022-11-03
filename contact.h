e82f288d44243b2e0e4a11e7a2969cc219e34ba7
yash-b1202 <yash.bhandari1202@gmail.com>
2022-11-03 10:57:51 +0530

    basic setup

---
+++ contact.h
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class contact{
+    private:
+    string f_name,l_name,phone_no;
+    
+    public:
+    contact(string phone, string f_name, string l_name){
+
+    }
+};
